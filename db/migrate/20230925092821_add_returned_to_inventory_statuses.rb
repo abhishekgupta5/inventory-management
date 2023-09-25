87763d71f9b9433c11a27b04cb61ab521cca7968
Abhishek <zenofabhishek@gmail.com>
2023-09-25 22:54:12 +0530

    [Migrations] Schema changes to support the order return flow
    1) Added a `returned` value for inventory_statuses enum as warehouse
       employees will be marking the inventory that's returned by
       the customer as `returned`.
       Enum values in postgres can't be dropped. We accept this
       tradeoff as `returned` is a pretty common status in  eCommerce
       systems and we should not need to remove it in any scenario
    
    2) Added a state enum on orders with possible values -
       `in_progress`, `fulfilled`, `returned`
       We already keep some sort of order_status in the system
       but I'd prefer to have maximum of this as precomputed
       instead of on-the-fly query.
       One more reason to have this precomputed is we want to
       keep count of returned orders. After returns, warehouse
       employees will move the returned inventory back to shelf (on_shelf)
    
       If we don't precompute that an order is `returned`, it
       will be difficult to list these orders for customer
       service team.
    
       With this, we'll also not need some of the scopes and status helper
       methods in the `order` model
    
    3) A data migration has been added which updates the `state` for
       each order in the system.
       Note: At this point of time, we don't have the `returned` state
       so we only mark orders as `fulfilled` or `in_progress`

---
+++ db/migrate/20230925092821_add_returned_to_inventory_statuses.rb
@@ -0,0 +1,16 @@
+class AddReturnedToInventoryStatuses < ActiveRecord::Migration[7.0]
+  def up
+    execute <<-SQL
+      ALTER TYPE inventory_statuses ADD VALUE 'returned';
+    SQL
+  end
+
+  def down
+    # Removing an enum value isn't supported in Postgres
+    # If we really want to drop the value, the only way is to
+    # remove the enum and add a new type with the values we want
+    #
+    # For simplicity, we can assume here that we won't need to
+    # remove the `returned` value
+  end
+end
---
+++ db/migrate/20230925105750_add_order_state_enum.rb
@@ -0,0 +1,28 @@
+class AddOrderStateEnum < ActiveRecord::Migration[7.0]
+  def up
+    execute <<-SQL
+      CREATE TYPE order_states AS ENUM ('in_progress', 'fulfilled', 'returned');
+    SQL
+    add_column :orders, :state, :order_states, null: false, default: 'in_progress'
+    update_state_for_orders
+  end
+
+  def down
+    remove_column :orders, :state
+    execute <<-SQL
+      DROP TYPE order_states
+    SQL
+  end
+
+  def update_state_for_orders
+    Order.find_each do |order|
+      state = case
+              when order.fulfilled?
+                'fulfilled'
+              else
+                'in_progress' # Fulfillable or unfulfillable
+              end
+      order.update!(state:)
+    end
+  end
+end
